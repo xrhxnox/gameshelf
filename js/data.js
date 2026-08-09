fc3b730b13d5a05d762f684b62c701207c2a10d8
Rix <nyjgtv47cn@privaterelay.appleid.com>
2026-08-08 20:57:56 -0700

    Editar: Metal Slug X

--- js/data.js
+++ js/data.js
@@ -1034,7 +1034,7 @@ const entries = [
     imagen: "assets/img/metal-slug-x-128.jpg",
     plataformas: ["switch","pc","arcade"],
     generos: ["Acción","Plataformas","Shooter","Arcade"],
-    top: "top10",
+    top: "top20",
     puntuacion: 5,
     fecha: "2026-08-01"
   },
