3176417d8f4ed8b6de8a414407710b06f7170b0c
Rix <nyjgtv47cn@privaterelay.appleid.com>
2026-08-08 21:52:44 -0700

    Editar: League of Legends

--- js/data.js
+++ js/data.js
@@ -37,7 +37,7 @@ const entries = [
     imagen: "assets/img/league-of-legends-3.jpg",
     plataformas: ["pc"],
     generos: ["Estrategia","MOBA"],
-    top: "top15",
+    top: "top20",
     puntuacion: 0,
     fecha: "2026-08-01"
   },
