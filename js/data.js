b9b162c881b7cff22d92bc686eff7cd8d05e4b67
Rix <nyjgtv47cn@privaterelay.appleid.com>
2026-08-09 04:06:15 -0700

    Agregar: Banjo-Kazooie

--- js/data.js
+++ js/data.js
@@ -1390,5 +1390,14 @@ const entries = [
     generos: ["Aventura","Plataformas"],
     puntuacion: 4,
     fecha: "2026-08-09"
+  },
+  {
+    id: 168,
+    titulo: "Banjo-Kazooie",
+    imagen: "assets/img/banjo-kazooie-168.jpg",
+    plataformas: ["n64"],
+    generos: ["Aventura","Plataformas"],
+    puntuacion: 4.5,
+    fecha: "2026-08-09"
   }
 ];
